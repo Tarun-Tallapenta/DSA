2305966c4d2c3de3e9620a063810a9f8cf9678d7
Tarun Tallapenta <o220132@rguktong.ac.in>
2026-07-11 18:06:14 +0530

    Add files via upload

---
+++ Arrays/sum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class sum{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int[] arr=new int[5];
+    System.out.println("enter the array elements");
+    for(int i=0;i<arr.length;i++){
+      arr[i]=sc.nextInt();
+    }
+    int sum=0;
+    for(int i=0;i<arr.length;i++){
+      arr[i]=sum+arr[i];
+      sum=arr[i];
+    }
+    System.out.println("sum of the elements of this array is " + sum);
+  
+    
+  }
+  
+}
