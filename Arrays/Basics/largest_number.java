7fd44b88091a46308706bc49c2581c1027352078
Tarun Tallapenta <o220132@rguktong.ac.in>
2026-07-12 10:49:11 +0530

    Add files via upload

---
+++ Arrays/Basics/largest_number.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class largest_number{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int[] arr=new int[5];
+    System.out.println("enter the array elements");
+    for(int i=0;i<arr.length;i++){
+      arr[i]=sc.nextInt();
+    }
+      
+     int largest=arr[0];
+    for(int i=0;i<arr.length;i++){
+      if(largest<arr[i]){
+        largest=arr[i];
+      }
+        }
+System.out.println("Largest number of this array is" + largest);
+System.out.println("Smallest number of this array is" + arr[0] );
+
+  
+  }
+
+}
